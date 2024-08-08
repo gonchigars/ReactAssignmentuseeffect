08e3432fd64884828a8b7b78e3408ff2d71729c7
Sarvesh Bramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-08 20:29:34 +0530

    Update Add.js

--- Add.js
+++ Add.js
@@ -1,1 +1,26 @@
+// src/App.js
+import React, { useState } from 'react';
+import WeatherWidget from './WeatherWidget';
+import './App.css';
+
+const App = () => {
+  const [city, setCity] = useState('London');
+
+  return (
+    <div className="App">
+      <div className="card">
+        <h1>Weather App</h1>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter city"
+        />
+        <WeatherWidget city={city} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
 
