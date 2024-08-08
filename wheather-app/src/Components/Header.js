fba747c631cf2affe997e0ad8ac1f596258ecde8
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-08-08 22:18:31 +0530

    MANI

---
+++ wheather-app/src/Components/Header.js
@@ -0,0 +1,13 @@
+// src/Header.js
+import React from 'react';
+import './Header.css'; // Importing styles for the header
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Weather App</h1>
+    </header>
+  );
+};
+
+export default Header;
