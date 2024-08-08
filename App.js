363750fbec2f05c8a01269270e28a1465754fcab
SriSankeerthanaGorrela <112100515+SriSankeerthanaGorrela@users.noreply.github.com>
2024-08-08 18:14:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+
+import VerticalButtons from './ass1';
+import HorizontalButtons from './ass';
+
+function App(){
+  return( <div className='app'>
+    <VerticalButtons/>
+    <HorizontalButtons/>
+  
+  </div>
+  );
+}
+export default App;
