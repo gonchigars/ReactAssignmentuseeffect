dff6aa9823e5735dc60fc751df948be2529dfdd1
SriSankeerthanaGorrela <112100515+SriSankeerthanaGorrela@users.noreply.github.com>
2024-08-08 18:19:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherWidget from './weatherwidget';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WeatherApp</h1>
+      <WeatherWidget/>
+        
+    </div>
+  
+) ;
+}
+
+export default App;
